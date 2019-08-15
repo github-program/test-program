52a16386e4767bc54fdc34c773bb827aa39af1a4
mythili1234 <48542779+mythili1234@users.noreply.github.com>
2019-08-14 23:54:30 -0700

    Create no8.c

---
+++ set5/no8.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int sum,a,b,c,d,e;
+    printf("enter the value a,b,c,d,e");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    sum=(a+b+c+d+e)/2;
+    printf("%d is avg",sum);
+}
