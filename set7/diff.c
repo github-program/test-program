86d8438a5c562a2df36d34ab2078b4ef608c5a7b
mythili1234 <48542779+mythili1234@users.noreply.github.com>
2019-08-16 07:20:21 -0700

    Create diff.c

---
+++ set7/diff.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int c,b,sum;
+printf("Enter number of elements c and b");
+scanf("%d%d", &c,&b);
+sum=a-b;
+if(sum%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
